9c05ac1da8fef65eba27f1c54b87df6b53252297
munderr <mvyazov534@gmail.com>
2025-02-19 22:44:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,9 @@
 
 int gcd(int a, int b) {
   while (b != 0){
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }
